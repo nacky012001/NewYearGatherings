72423e64e8091fcc74fb689ef225b36a0ea2fe3d
Thomas un <kong19881230@gmail.com>
2019-01-26 13:16:01 +0800

    add palyer controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float playerSpeed = 10; //speed player moves
+   
+   public void  Update()
     {
-        
+
+        MoveForward(); // Player Movement 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveForward()
     {
-        
+
+        if (Input.GetKey("up"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("down"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("left"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("right"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
+
+
---
+++ Assets/main.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using System.IO;
+using System.Diagnostics;
+using System; //#1
+public class main : MonoBehaviour
+{
+    public Text timec;
+    public Text ScoreText; //宣告一個ScoreText的text
+    public int Score = 0;
+    private string min;
+    private string sec;
+    public float temp;
+    // Use this for initialization
+    void Start()
+    {
+        temp = 3660f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        times();
+        temp0();
+
+    }
+
+    public void gostate1()
+    {
+        SceneManager.LoadScene("state1");
+    }
+    public void times()
+    {
+        min = Mathf.Floor(temp / 60 / 60f).ToString("00");
+        sec = Mathf.Floor(temp / 60 % 60f).ToString("00");
+        timec.text = String.Format("{0:D}:{1:D}", min, sec);
+    }
+
+    public void temp0()
+    {
+
+        if (temp > 0)
+        {
+            temp = temp - 1f;
+        }
+    }
+    public void AddScore()
+
+    {
+
+        Score += 10; //分數+10
+
+        ScoreText.text = "Score: " + Score; // 更改ScoreText的內容
+    }
+    public void ReduceScore()
+
+    {
+
+        Score -= 10; //分數+10
+
+        ScoreText.text = "Score: " + Score; // 更改ScoreText的內容
+    }
+
+}
